022903439ea791cbc2cbcd0524176ab3869bb81d
Praveen <praveen.jaisingh@cloudrevelinnovation.com>
2026-06-09 15:05:37 +0530

    code updated

--- src/Middleware/cspMiddleware.js
+++ src/Middleware/cspMiddleware.js
@@ -1,17 +1,17 @@
-const helmet = require("helmet");
+// const helmet = require("helmet");
 
-const cspMiddleware = helmet.contentSecurityPolicy({
-    directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        imgSrc: ["'self'", "data:", "blob:"],
-        fontSrc: ["'self'", "data:"],
-        connectSrc: ["'self'"],
-        objectSrc: ["'none'"],
-        frameAncestors: ["'none'"],
-        upgradeInsecureRequests: []
-    }
-});
+// const cspMiddleware = helmet.contentSecurityPolicy({
+//     directives: {
+//         defaultSrc: ["'self'"],
+//         scriptSrc: ["'self'", "'unsafe-inline'"],
+//         styleSrc: ["'self'", "'unsafe-inline'"],
+//         imgSrc: ["'self'", "data:", "blob:"],
+//         fontSrc: ["'self'", "data:"],
+//         connectSrc: ["'self'"],
+//         objectSrc: ["'none'"],
+//         frameAncestors: ["'none'"],
+//         upgradeInsecureRequests: []
+//     }
+// });
 
-module.exports = cspMiddleware;
+// module.exports = cspMiddleware;
