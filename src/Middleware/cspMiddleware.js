91f08916016404d6acd2046ef3fde77f00f8862c
Praveen <praveen.jaisingh@cloudrevelinnovation.com>
2026-06-09 14:57:39 +0530

    code updated

--- src/Middleware/cspMiddleware.js
+++ src/Middleware/cspMiddleware.js
@@ -3,25 +3,11 @@ const helmet = require("helmet");
 const cspMiddleware = helmet.contentSecurityPolicy({
     directives: {
         defaultSrc: ["'self'"],
-        scriptSrc: [
-            "'self'"
-        ],
-        styleSrc: [
-            "'self'",
-            "'unsafe-inline'"
-        ],
-        imgSrc: [
-            "'self'",
-            "data:",
-            "blob:"
-        ],
-        fontSrc: [
-            "'self'",
-            "data:"
-        ],
-        connectSrc: [
-            "'self'"
-        ],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "blob:"],
+        fontSrc: ["'self'", "data:"],
+        connectSrc: ["'self'"],
         objectSrc: ["'none'"],
         frameAncestors: ["'none'"],
         upgradeInsecureRequests: []
